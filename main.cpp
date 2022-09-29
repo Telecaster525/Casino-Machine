6b5894572978d77300fab2fae2adea747a01ef09
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-09-29 16:16:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int s1, s2, s3, inf, score = 0;
     char start;
+    int trying = 0;
     inf = 1;
     cout << "Welcome to Casino I/iTMO !" << " \n";
     cout << "Created by @bloom_guy" << " \n";
@@ -24,12 +25,18 @@ int main() {
             if (start == 's' && s1 == s2 && s2 == s3) {
                 cout << "$$$_JACKPOT_$$$" << " \n";
                 score = ++score;
+                trying = ++trying;
+                
             }
             if (start == 's' && (s1 != s2 || s2 != s3)) {
                 cout << "You suck" << " \n";
+                trying = ++trying;
             }
             if (start == 'b') {
+                cout << "------------" << " \n";
                 cout << "Your score:" << score << " \n";
+                cout << "Your attemps:" << trying << " \n";
+                cout << "------------" << " \n";
             }
         } while (inf > 0);
     }
