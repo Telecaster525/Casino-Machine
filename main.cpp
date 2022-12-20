0e0bfa010c721c6b272c6c9cf72390fda9fdd23e
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-12-20 13:26:36 +0300

    V1.2

--- main.cpp
+++ main.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    int s1, s2, s3, inf, score = 0;
+    bool infinity = true;
+    int s1, s2, s3;
+    int score = 0, trying = 0;
     char start;
-    int trying = 0;
-    inf = 1;
-    cout << "Welcome to Casino I/iTMO !" << " \n";
-    cout << "Created by @bloom_guy" << " \n";
-    cout << "Your score:" << score << " \n";
-    cout << "To see your score enter \"b\"" << " \n";
+    std::cout << "Welcome to Casino \"I/iTMO\"" << " \n";
+    std::cout << "Your score: " << score << " \n";
+    std::cout << "If you need help (commands) enter \"h\"" << " \n";
     for(;;) {
         do {
-            cout << "Enter \"s\" to spin:" << " \n";
-            cin >> start;
+            std::cin >> start;
+            if (start == 'q') {
+                std::cout << "Game Over" << " \n";
+                std::cout << "Created by @bloom_guy" << std::endl;
+                return 0;
+            }
+            if (start == 'h') {
+                std::cout << "To spin enter \"s\"" << " \n";
+                std::cout << "To see your score enter \"b\"" << " \n";
+                std::cout << "To stop the game enter \"q\"" << " \n";
+            }
             if (start == 's') {
-                s1 = rand() % 4;
-                s2 = rand() % 4;
-                s3 = rand() % 4;
-                cout << s1 << ' ';
-                cout << s2 << ' ';
-                cout << s3 << " \n";
+                s1 = 1+rand() % 3;
+                s2 = 1+rand() % 3;
+                s3 = 1+rand() % 3;
+                std::cout << '[' << s1 << '-' << s2 << '-' << s3 << ']' <<" \n";
             }
             if (start == 's' && s1 == s2 && s2 == s3) {
-                cout << "$$$_JACKPOT_$$$" << " \n";
+                std::cout << "$$$$$ JACKPOT $$$$$" << " \n";
                 score = ++score;
                 trying = ++trying;
-                
+
             }
             if (start == 's' && (s1 != s2 || s2 != s3)) {
-                cout << "You suck" << " \n";
+                std::cout << "You suck!" << " \n";
                 trying = ++trying;
             }
             if (start == 'b') {
-                cout << "------------" << " \n";
-                cout << "Your score:" << score << " \n";
-                cout << "Your attemps:" << trying << " \n";
-                cout << "------------" << " \n";
+                std::cout << "------------" << " \n";
+                std::cout << "Your score: " << score << " \n";
+                std::cout << "Your attemps: " << trying << " \n";
+                std::cout << "------------" << " \n";
             }
-        } while (inf > 0);
+        } while (infinity);
     }
     return 0;
 }
